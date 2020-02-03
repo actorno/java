7a96ad9198cac5a6c61ff66393a196cf27caa28e
Aerhelle <aerhelle@gmail.com>
2020-02-03 13:10:38 -0500

    Java Project first commit

---
+++ Search.java
@@ -0,0 +1,14 @@
+class Search
+{
+    static int search(int[] arr, int x){
+        for (int k = 0; k < arr.length; k++){
+            if (arr[k] == x)
+            return k;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {4,2,6,42};
+        System.out.print(search(arr,42));
+    }
+}
